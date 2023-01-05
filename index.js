5379416ac9e5661bb4e869b453990a706d4247be
Exqweezey <exqweezey337@mail.ru>
2023-01-05 21:52:10 +0300

    init

--- index.js
+++ index.js
@@ -6,26 +6,7 @@ const token = '5334315426:AAG7ByyVXdWhctYK6WzQp57thFVyD5F-_Nc'
 
 const bot = new TelegramApi(token, {polling: true})
 
-const gameOptions = {
-	reply_markup: JSON.stringify({
-		inline_keyboard: [
-			[{text: '1', callback_data: '1'}, {text: '2', callback_data: '2'}, {text: '3', callback_data: '3'}],
-			[{text: '4', callback_data: '4'}, {text: '5', callback_data: '5'}, {text: '6', callback_data: '6'}],
-			[{text: '7', callback_data: '7'}, {text: '8', callback_data: '8'}, {text: '9', callback_data: '9'}],
-			[{text: '0', callback_data: '0'}],
 
-			
-		]
-	})
-}
-
-const againOptions = {
-	reply_markup: JSON.stringify({
-		inline_keyboard: [
-			[{text: 'Играть снова', callback_data: '/again'}],
-		]
-	})
-}
 
 const startGame = async (chatId) => {
 	await bot.sendMessage(chatId, `Сейчас я загадаю число от 0 до 9, а ты должен угадать его`)
